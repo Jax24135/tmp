247aa3c40a460dc5d675f0a890f05d2c0a26379a
Jon Jackson <Jax24135@yahoo.com>
2017-09-06 22:35:03 -0500

    5 12 2005 Thursday
    modified on Wednesday night to
    almost have it working except "5 12 2005" >> Thursday

--- openLab1.cpp
+++ openLab1.cpp
@@ -1,4 +1,14 @@
-#include<iostream>
+/*
+PROGRAMMER: Jonathan Jackson
+Assignment: which assignment is this? OLA 1
+Class: CSCI 2170, Section 1
+Course Instructor: Dr. Li
+Due Date: Midnight, Monday, 9/7/2017
+Description:   
+*/
+
+#include <iostream>
+#include <string>
 using namespace std;
 
 // Global Constants
@@ -9,85 +19,115 @@ bool CheckDayValidity(int);
 bool CheckYearValidity(int);
 
 int CorrectMonth(int);
+int CorrectYear(int, int);
 
 int main()
 {
-    int month; // original User input month
-    int day;   // original User input day
-    int year;  // original User input year
-    int f;          // resulting day of the week
-    int d;          // day of the month
-    int m;          // month number (see Zeller's Rule)
-    int D;          // decade; last 2 digits of the modified year
-    int C;          // century; first 2 digits of the modified year 
+    int month; // User input month
+    int day;   // User input day
+    int year;  // User input year
+    int f;     // resulting day of the week
+    int d;     // day of the month
+    int m;     // month number (see Zeller's Rule)
+    int D;     // decade; last 2 digits of the modified year
+    int C;     // century; first 2 digits of the modified year 
     int adjustedMonth;  //month adjusted for Zeller Rule
-
+    int adjustedYear;   //year adjusted for Zeller Rule
+    string resultingDay;   //day of the week User was born on
+    
     cout << "Welcome ! \n"
     << "Just tell me your birth date, and I will tell which day of the week you were born. \n"
     << "Lets get started ...\n\n"
     << "Please tell me your birth date, in the form of: month day year\n";
-        
+    
+    // accpet input from User setting variables for month, day & year
     cin >> month >> day >> year;
     
     // verify dates are in possible ranges && NOT negative
-    if ((CheckMonthValidity == false) || (CheckDayValidity == false) || (CheckYearValidity == false))
+    // i.e. "month" between 1 && 12; "day" between 1 && 31; Year non-negative && below 2016
+    
+    
+    //if ((CheckMonthValidity == false) || (CheckDayValidity == false) || (CheckYearValidity == false))
+    if((month < 1) || (month>12) || (day<1) || (day>31) || (year<0) || (year>2016))
         {
-            cout << "Your input is not correct.\n"
-            << "The computation is not carried out.\n\n";
+            cout << "Your input is not correct.\n";
+            cout << "The computation is not carried out.\n\n";
         }
     else
         {
-        cout << endl << "yep it's valid\n\n";
-        //adjustedMonth = CorrectMonth(month);
-        //cout << adjustedMonth;
-        //run through Zeller's with proper output
-        // f = day + (13*month-1)/5 + D + D/4 + C/4 - 2*C
-        D = year % 100;
-        cout << D <<" << this is D\n\n";
-        C = year/100;
-        cout << C << " << this is C\n";
-        }
+        // fix "month" to Zeller Rule (Jan=11, Feb=12,Mar=1...)
+        adjustedMonth = CorrectMonth(month);
+        // use adjustedMonth to determine if year needs adjustment as well.
+        adjustedYear = CorrectYear(year, adjustedMonth);
+       
+        D = adjustedYear % 100;
+        //cout << "D is " << D << endl;
+        
+        C = adjustedYear / 100;
+        //cout << "C is " << C << endl << endl;
+        
+        // Zeller's Algorithm          
+        f = day + ((13*adjustedMonth-1)/5) + D + (D/4) + (C/4) - (2*C);
+
+        if (f%7 == 0)
+            {
+                resultingDay = "Sunday";
+            }
+        else if (f%7 == 1)
+            {
+                resultingDay = "Monday";
+            }
+        else if (f%7 == 2)
+            {
+                resultingDay = "Tuesday";
+            }
+        else if (f%7 == 3)
+            {
+                resultingDay = "Wednesday";
+            }
+        else if (f%7 == 4)
+            {
+                resultingDay = "Thursday";
+            }
+        else if (f%7 == 5)
+            {
+                resultingDay = "Friday";
+            }
+        else if (f%7 == 6)
+            {
+                resultingDay = "Saturday";
+            }
+        
+        cout << "You were born on " << resultingDay << ".\n";
+    }
     
     return 0;
-}
-
-bool CheckMonthValidity(int month)
-{
-    if ((month > 0) && (month < 13))
-        {return true;}
-    else
-        {return false;}
-}
-
-bool CheckDayValidity(int day)
-{
-    if ((day > 0) && (day < 32))
-        {return true;}
-    else
-        {return false;}
-}
-
-bool CheckYearValidity(int year)
-{
-    if ((year > 0) && (year < 2017))
-    {return true;}
-    else
-    {return false;}
 }
 
 int CorrectMonth(int month)
 {
     if(month > 2)
         {
-            return (month -= 2);
+            month -= 2;
         }
     else if(month == 1)
         {
-            return (month = 11);
+            month = 11;
         }
     else if(month == 2)
         {
-            return (month = 12);
+            month = 12;
         }
+    return month;
 
+}
+
+int CorrectYear(int year, int adjustedMonth)
+{
+    if (adjustedMonth > 10)
+        {
+            year = year-1;
+        }
+    return year;
+    
 }
