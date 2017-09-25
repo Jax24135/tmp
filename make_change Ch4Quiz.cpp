5bd6d5f195d04d3a991d7ab47ebb6bb90dfc344f
Jon Jackson <Jax24135@yahoo.com>
2017-09-24 22:08:09 -0500

    Create make_change Ch4Quiz.cpp

---
+++ make_change Ch4Quiz.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int VerifyChange (int cointValue);
+void computeCoin(int &cointValue,int &number, int &amountLeft, int &quarters, int &dimes, int &pennies);
+void PrintResults(int &quarters,int &dimes, int &pennies);
+
+int main()
+{
+    
+    int cointValue;
+    int number;
+    int amountLeft;
+    int quarters;
+    int dimes;
+    int pennies;
+	
+	cointValue = VerifyChange(cointValue);
+	computeCoin(cointValue, number, amountLeft, quarters, dimes, pennies);
+	PrintResults(quarters, dimes, pennies);
+}
+
+
+int VerifyChange (int cointValue) {
+	cout << "Enter the amount of change:";
+	cin >> cointValue;
+	
+	while (cointValue < 1 || cointValue > 99) {
+		cout << "Enter a value between 1 and 99:";
+		cin >> cointValue;
+	}
+	return cointValue;
+}
+
+void computeCoin(int &cointValue, int &number, int &amountLeft, int &quarters, int &dimes, int &pennies) {
+    amountLeft = cointValue;
+    
+    cout << "amount left is " << amountLeft << endl;
+    
+    if (amountLeft > 24) {
+        quarters = (amountLeft / 25);
+        amountLeft = (amountLeft % 25);
+    }
+    
+    if (amountLeft > 9) {
+        dimes = (amountLeft / 10);
+        amountLeft = (amountLeft % 10);
+    }
+    
+    pennies = amountLeft;
+}
+
+void PrintResults(int &quarters,int &dimes, int &pennies) {
+    cout << "Change can be given as:\n"
+         << quarters << " quarter(s)\n"
+         << dimes << " dime(s)\n"
+         << pennies << " penny (pennies)\n";    
+}
