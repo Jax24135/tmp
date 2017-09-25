4459bd5c6bc5835608cdc2268a0be988d41ffb66
Jon Jackson <Jax24135@yahoo.com>
2017-09-24 23:17:45 -0500

    Update make_change Ch4Quiz.cpp
    Fixed/Solved

--- make_change Ch4Quiz.cpp
+++ make_change Ch4Quiz.cpp
@@ -1,47 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int VerifyChange (int cointValue);
+void VerifyChange (int &cointValue);
 void computeCoin(int &cointValue,int &number, int &amountLeft, int &quarters, int &dimes, int &pennies);
 void PrintResults(int &quarters,int &dimes, int &pennies);
 
 int main()
 {
-    
-    int cointValue;
-    int number;
-    int amountLeft;
-    int quarters;
-    int dimes;
-    int pennies;
+    //int cointValue = 0, number=0, amountLeft=0, quarters=0, dimes=0, pennies=0;
+    char runAgain = 'Y';
 	
-	cointValue = VerifyChange(cointValue);
+    while (runAgain == 'Y') {
+        int cointValue = 0, number=0, amountLeft=0, quarters=0, dimes=0, pennies=0;
+	VerifyChange(cointValue);
 	computeCoin(cointValue, number, amountLeft, quarters, dimes, pennies);
 	PrintResults(quarters, dimes, pennies);
+        
+        cout << "Do you want to make change again?\n"
+             << "Enter Y to convert again and any other key to exit:";
+        cin >> runAgain;
+    }
+        
 }
 
 
-int VerifyChange (int cointValue) {
+void VerifyChange (int &cointValue) {
 	cout << "Enter the amount of change:";
 	cin >> cointValue;
 	
 	while (cointValue < 1 || cointValue > 99) {
 		cout << "Enter a value between 1 and 99:";
 		cin >> cointValue;
-	}
-	return cointValue;
+        }
 }
 
 void computeCoin(int &cointValue, int &number, int &amountLeft, int &quarters, int &dimes, int &pennies) {
     amountLeft = cointValue;
     
-    cout << "amount left is " << amountLeft << endl;
-    
     if (amountLeft > 24) {
         quarters = (amountLeft / 25);
         amountLeft = (amountLeft % 25);
     }
-    
     if (amountLeft > 9) {
         dimes = (amountLeft / 10);
         amountLeft = (amountLeft % 10);
